035a163b3806fd48eb90a1d67746e9f410b7f179
superhq <hongqun@126.com>
2019-07-17 16:44:07 +0800

    my spring shiro module, authentication and authorization.

---
+++ my-spring-shro-quickstart/src/test/java/my/spring/myspringshroquickstart/MySpringShroQuickstartApplicationTests.java
@@ -0,0 +1,16 @@
+package my.spring.myspringshroquickstart;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MySpringShroQuickstartApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
