93b05eba7a94f5e9f20bd1e207774387b82e1599
hongqun <hongqun@126.com>
2020-02-18 23:17:27 +0800

    自定义用户名/密码

--- my-security/src/main/java/com/example/mysecurity/conf/MySecurityConf.java
+++ my-security/src/main/java/com/example/mysecurity/conf/MySecurityConf.java
@@ -1,15 +1,37 @@
 package com.example.mysecurity.conf;
 
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.boot.autoconfigure.security.servlet.SpringBootWebSecurityConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class MySecurityConf extends WebSecurityConfigurerAdapter {
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    public MySecurityConf(PasswordEncoder passwordEncoder){
+        this.passwordEncoder = passwordEncoder;
+    }
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails userDetails = User
+                .builder()
+                .username("admin")
+                .password(passwordEncoder.encode("password"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
@@ -20,4 +42,5 @@ public class MySecurityConf extends WebSecurityConfigurerAdapter {
                 .and()
                 .httpBasic();
     }
+
 }
